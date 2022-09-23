9f5b0c19214c853c3c00912a957b9f7917550293
Moo Olaniyan <moo@circleci.com>
2022-09-23 16:35:34 -0400

    test splitting

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ const html = fs.readFileSync(path.resolve(__dirname,"../templates/index.html"), 
 jest
     .dontMock('fs');
 
-describe("Button Test", function() {
+describe("Button Gray Test", function() {
     beforeEach(() => {
 	const openSesame = require('../js/click-script');
         document.body.innerHTML = html.toString();
@@ -18,7 +18,22 @@ describe("Button Test", function() {
     afterEach(() => {
         jest.resetModules();
     });
-    it("Button Change Test", function() {
+    it("Button Gray Test", function() {
         expect(global.document.getElementById('clickButton').disabled).toBe(true);
+    });
+});
+describe("Button Click Test", function() {
+    beforeEach(() => {
+	const openSesame = require('../js/click-script');
+        document.body.innerHTML = html.toString();
+//	document.getElementById('clickButton').click();
+	openSesame();
+	const clickMessage = document.getElementById('clickMessage').innerHTML;
+    });
+    afterEach(() => {
+        jest.resetModules();
+    });
+    it("Button Click Test", function() {
+        expect(clickMessage.innerHTML).toBe("Thank you!!");
     });
 });
