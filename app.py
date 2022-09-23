e649e35ad3e871ecf61b5663c41bc85244dfbd0a
EC2 Default User <ec2-user@ip-172-31-10-109.ec2.internal>
2022-09-23 17:37:23 +0000

    added new homnelab-deploy branch

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from views import views
-import os
-
-app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/")
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=os.getenv('PORT') or '8000')
