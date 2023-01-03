35ef0eb570e93b99e24c2fc2054b483b99c20325
Moo Olaniyan <moo@circleci.com>
2023-01-03 13:40:10 -0500

    readable trademark and more descriptive button text

--- static/click-script.js
+++ static/click-script.js
@@ -1,6 +1,6 @@
 // This function inserts the text upon being called, and then disables the click button
 const openSesame = () => {
-    var text = "Thank you!!";
+    var text = "Thank you! This button click was tested by our pipeline!";
     document.getElementById("clickMessage").innerHTML = text;
     document.getElementById("clickButton").disabled = true;
 }
